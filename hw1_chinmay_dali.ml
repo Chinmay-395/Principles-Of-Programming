69480685e2831a11bdedebcd4c8cbb091333c658
Chinmay Dali <chinmaydali395@gmail.com>
2022-09-15 13:31:12 -0400

    fixed repeat question wrt parameter

--- hw1_chinmay_dali.ml
+++ hw1_chinmay_dali.ml
@@ -39,17 +39,45 @@ let rec rotate_90_word l =
   | [] -> []
   | h::t -> rotate_90_letter h :: rotate_90_word t;;
 
-let rec repeat n x = 
-  match n with
-  | 0 -> []
-  | n -> x:: repeat x (n-1);;
+let rec repeat: int->'a->'a list = 
+  fun n x ->
+  if n==0 then [] else x:: repeat (n-1) x;;
 
 (* 
 Pantograph
 [0;2;2;3;3;5;5;4;3;5;4;3;3;5;5;1]
+*)
 
-[0; 2; 2; 2; 2; 3; 3; 3; 3; 5; 5; 5; 5; 4; 4; 3; 3; 5; 5; 4; 4; 3; 3;4 3; 3; 5; 5; 5; 5; 1] *)
 
+(* Store a sublist like 
+ 2, 2 --> 2,2,2,2
+ [0;2;2;3] --> [0; 2;2;2;2; 3;3]
+number of repeated numbers multipled by n-times
+
+*)
+(* Attempt 1 *)
+let replicate list n =
+    let rec prepend n acc x =
+      if n = 0 then acc else prepend (n-1) (x :: acc) x in
+    let rec aux acc = function
+      | [] -> acc
+      | h :: t -> aux (prepend n acc h) t  in
+    (* This could also be written as:
+       List.fold_left (prepend n) [] (List.rev list) *)
+    aux [] (List.rev list);;
+(* let rec pantograph: int -> int list -> int list =
+  
+  fun num l ->
+    match l with
+    | [] -> []
+    | h1::h2::t when (h1==0 || h1==1) -> pantograph num (h2::t) 
+    | h1::h2::t when (h1==h2) -> repeat h1 (num*(i+1)) 
+    | h1::h2::t when (h1<>h2) -> pantograph num (h2::t) 
+    | _ -> failwith "An issue";; *)
+
+    
+
+    
 (* Question 6
 Implement a function
 coverage : int*int -> int list -> (int*int) list
@@ -64,7 +92,10 @@ Also, you need not concern yourself with repetitions. For example:
 
 (* Need to keep track of the rows and column  *)
 let coverage : int*int -> int list -> (int*int) list =
+  
   fun startCor l ->
+    let initial_list = [startCor]
+    
     match l with
     | [] -> []
-    | h::t -> failwith "qweqwe"
+    | h::t -> 
