82712d08bd4d27c10e744bf7987f407683b5ca99
Chinmay Dali <chinmaydali395@gmail.com>
2022-09-15 17:29:27 -0400

    The pantograph_nm functionality added

--- hw1_chinmay_dali.ml
+++ hw1_chinmay_dali.ml
@@ -43,41 +43,27 @@ let rec repeat: int->'a->'a list =
   fun n x ->
   if n==0 then [] else x:: repeat (n-1) x;;
 
-(* 
-Pantograph
-[0;2;2;3;3;5;5;4;3;5;4;3;3;5;5;1]
-*)
 
 
-(* Store a sublist like 
- 2, 2 --> 2,2,2,2
- [0;2;2;3] --> [0; 2;2;2;2; 3;3]
-number of repeated numbers multipled by n-times
 
-*)
-(* Attempt 1 *)
-
-    
-let pantograph_nm list n =
+let pantograph_nm n list =
     let rec prepend n acc x =
       if n = 0 then acc else prepend (n-1) (x :: acc) x in
-    let rec aux acc = 
-      function
-      | [] -> acc
-      | h :: t -> if (h<>0 && h<>1) then aux (prepend n acc h) t else aux (prepend 1 acc h) t
-      in aux [] (List.rev list);;
+        let rec aux acc = 
+          function
+          | [] -> acc
+          | h :: t -> if (h<>0 && h<>1) then aux (prepend n acc h) t else aux (prepend 1 acc h) t
+          in aux [] (List.rev list);;
 
-    
-(* Question 6
-Implement a function
-coverage : int*int -> int list -> (int*int) list
-that given a starting coordinate and a program returns the list of coordinates that the
-program visits. You may introduce helper functions to make your code more readable.
-Also, you need not concern yourself with repetitions. For example:
-# coverage (0 ,0) letter_e ;;
-- : ( int * int ) list =
-[(0 , 0) ; (0 , 0) ; (0 , 1) ; (0 , 2) ; (1 , 2) ; (2 , 2) ; (1 , 2) ; (0 , 2) ; (0 , 1) ;
-(1 , 1) ; (0 , 1) ; (0 , 0) ; (1 , 0) ; (2 , 0) ; (1 , 0) ; (0 , 0) ; (0 , 0) ]   
-*)
+(* With map *)
 
-(* Need to keep track of the rows and column  *)
+(* let pantograph_nm n list =
+    let rec prepend n acc x =
+      if n = 0 then acc else prepend (n-1) (x :: acc) x in
+        let rec aux acc = 
+          function
+          | [] -> acc
+          | h :: t -> if (h<>0 && h<>1) then aux (prepend n acc h) t else aux (prepend 1 acc h) t
+          in aux [] (List.rev list);; *)
+
+          
